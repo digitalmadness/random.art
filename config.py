1c3013d52aecab841dbca5e167c078f98701aacc
exife <vl70x3@gmail.com>
2018-03-17 06:29:42 +0300

    add settings for autofollow addon

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import os
 import configparser
 import tweepy
-"""Reads configuration file and holds all settings for the bot to function."""
+import codecs
+
+"""reads configuration file and holds all settings for the bot to function. get out of here stalker, edit settings.txt"""
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 # read configs from file
 config = configparser.ConfigParser()
-config.read(dname + '/settings.txt')
+config.readfp(codecs.open('settings.txt','r',encoding='utf8'))
 twitter_config = config['Twitter']
 api_key = twitter_config['api_key']
 secret_key = twitter_config['secret_key']
@@ -21,6 +23,11 @@ chance = int(app_config['execution_chance'])
 interval = int(app_config['time_between_tweets'])
 saucenao_config = config['Saucenao']
 api_key_saucenao = saucenao_config['api_key_saucenao']
+autofollow_config = config['Autofollow']
+search_phrase = autofollow_config['search_phrase']
+custom_following_limit = autofollow_config['custom_following_limit']
+followback_opt = autofollow_config['follow_those_who_already_follow_you']
+like_opt = autofollow_config['like_all_found_tweets']
 
 auth = tweepy.OAuthHandler(api_key, secret_key)
 auth.set_access_token(token, secret_token)
