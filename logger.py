873ef54593d753d733f4504c2424bd52ea9d4e35
exife <36610721+exife@users.noreply.github.com>
2018-04-03 18:43:03 +0300

    who needs standart logger lib anyway

--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 import datetime
-"""These functions log everything. To-do: actually use the standar logger
-module."""
+
+"""who needs standart logger lib anyway"""
 
 date = str(datetime.datetime.now())
 
@@ -8,14 +8,3 @@ date = str(datetime.datetime.now())
 def addPost(img_path, reply_id, log):
     with open(log, 'a') as log:
         log.write(date + '\t' + str(img_path) + '\t' + str(reply_id) + '\n')
-
-
-def addBanned(reply_id, log):
-    ban_message = "ABOVE IMAGE WAS BANNED!"
-    with open(log, 'a') as log:
-        log.write(date + '\t' + ban_message + '\t' + str(reply_id) + '\n')
-
-
-def addWarning(warning, log):
-    with open(log, 'a') as log:
-        log.write(date + '\t' + warning + '\n')
